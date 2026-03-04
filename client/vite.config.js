833ee4124cdc1ed0cd15487052d4db830e0f5af9
Nilpete12 <nilpete12@gmail.com>
2026-03-04 20:17:31 +0530

    PWA updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), tailwindcss(),
+    react(), 
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate', // Automatically updates the app when you push new code
       includeAssets: ['favicon.svg', 'apple-touch-icon.png'], // Add your icons here
