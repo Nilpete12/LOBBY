dab0f4008714a814e5bc640d2c4c162fb34b8ac1
khalongkichu348-ops <khalongkichu348@gmail.com>
2026-06-30 20:42:16 +0530

    Replace homepage background image

--- lobby/src/components/Hero.jsx
+++ lobby/src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
 
       {/* Background gradients */}
       <div className="absolute inset-0 bg-linear-to-br from-[#DCFCE7]/40 via-[#F8FAFC] to-[#BFDBFE]/40"></div>
-      <div className="pointer-events-none absolute inset-0 bg-[url('/dzukou-valley.webp')] bg-cover bg-center opacity-[0.15] mix-blend-multiply saturate-125"></div>
+      <div className="pointer-events-none absolute inset-0 bg-[url('/homepage-city.jpg')] bg-cover bg-center opacity-[0.15] mix-blend-multiply saturate-125"></div>
 
       {/* Blur blobs */}
       <div className="absolute top-20 -left-20 w-96 h-96 bg-[#BBF7D0]/30 rounded-full blur-3xl"></div>
