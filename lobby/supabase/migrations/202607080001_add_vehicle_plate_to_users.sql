4f77b1a73ae76c810e3946a091598b09cddf81b0
Nilpete12 <nilpete12@gmail.com>
2026-07-17 23:28:43 +0530

    All migrations added upto date.

--- lobby/supabase/migrations/202607080001_add_vehicle_plate_to_users.sql
+++
@@ -1,6 +0,0 @@
-alter table public.users
-  add column if not exists vehicle_plate text;
-
-create index if not exists users_vehicle_plate_idx
-  on public.users (vehicle_plate)
-  where vehicle_plate is not null and vehicle_plate <> '';
--- lobby/supabase/migrations/202607090001_add_taxi_stands_to_users.sql
+++
@@ -1,5 +0,0 @@
-alter table public.users
-  add column if not exists taxi_stands text[] not null default '{}';
-
-create index if not exists users_taxi_stands_gin_idx
-  on public.users using gin (taxi_stands);
